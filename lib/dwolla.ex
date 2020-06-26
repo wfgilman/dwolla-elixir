5d4d0414981723842bf04ce42de7d88cc3aa9b3b
Christian Schuetz <cmschuetz12@gmail.com>
2020-06-25 22:32:30 -0500

    chore: Bump Poison to 4.0
    - Bumps Poison to 4.0
    - Updates APIs to work with the new version
    - Autoformatter changes

--- lib/dwolla.ex
+++ lib/dwolla.ex
@@ -11,40 +11,40 @@ defmodule Dwolla do
 
   defmodule MissingClientSecretError do
     defexception message: """
-    Client secret is missing. Please add client_id to your config.exs file.
+                 Client secret is missing. Please add client_id to your config.exs file.
 
-    config :dwolla, client_id: "your_client_id"
-    """
+                 config :dwolla, client_id: "your_client_id"
+                 """
   end
 
   defmodule MissingClientIdError do
     defexception message: """
-    Client Id is missing. Please add client_secret to your config.exs file.
+                 Client Id is missing. Please add client_secret to your config.exs file.
 
-    config :dwolla, client_secret: "your_client_secret"
-    """
+                 config :dwolla, client_secret: "your_client_secret"
+                 """
   end
 
   defmodule MissingRootUriError do
     defexception message: """
-    The root_uri is required to specify the Dwolla environment to which you are
-    making calls, i.e. development or production. Please configure
-    root_uri in your config.exs file.
+                 The root_uri is required to specify the Dwolla environment to which you are
+                 making calls, i.e. development or production. Please configure
+                 root_uri in your config.exs file.
 
-    config :dwolla, root_uri: "https://api-sandbox.dwolla.com/" (development)
-    config :dwolla, root_uri: "https://api.dwolla.com/" (production)
-    """
+                 config :dwolla, root_uri: "https://api-sandbox.dwolla.com/" (development)
+                 config :dwolla, root_uri: "https://api.dwolla.com/" (production)
+                 """
   end
 
   defmodule MissingOauthUriError do
     defexception message: """
-    The oauth_uri is required to specify the Dwolla environment to which you are
-    making authorization requests, i.e. development or production. Please configure
-    oauth_uri in your config.exs file.
+                 The oauth_uri is required to specify the Dwolla environment to which you are
+                 making authorization requests, i.e. development or production. Please configure
+                 oauth_uri in your config.exs file.
 
-    config :dwolla, oauth_uri: "https://sandbox.dwolla.com/oauth/v2/" (development)"
-    config :dwolla, oauth_uri: "https://www.dwolla.com/oauth/v2/" (production)
-    """
+                 config :dwolla, oauth_uri: "https://sandbox.dwolla.com/oauth/v2/" (development)"
+                 config :dwolla, oauth_uri: "https://www.dwolla.com/oauth/v2/" (production)
+                 """
   end
 
   @doc """
@@ -58,7 +58,7 @@ defmodule Dwolla do
   @doc """
   Gets root URI from configuration.
   """
-  @spec get_root_uri() :: String.t | no_return
+  @spec get_root_uri() :: String.t() | no_return
   def get_root_uri do
     require_root_uri()
   end
@@ -66,8 +66,8 @@ defmodule Dwolla do
   @doc """
   Makes request with token.
   """
-  @spec make_request_with_token(atom, String.t, String.t, map, map, Keyword.t) ::
-    {:ok, HTTPoison.Response.t} | {:error, HTTPoison.Error.t | {:invalid, binary}}
+  @spec make_request_with_token(atom, String.t(), String.t(), map, map, Keyword.t()) ::
+          {:ok, HTTPoison.Response.t()} | {:error, HTTPoison.Error.t() | {:invalid, binary}}
   def make_request_with_token(method, endpoint, token, body \\ %{}, headers \\ %{}, options \\ []) do
     rb = body |> Utils.to_camel_case() |> maybe_encode()
     rh = token |> get_request_headers() |> Map.merge(headers) |> Map.to_list()
@@ -82,15 +82,17 @@ defmodule Dwolla do
   Makes request to OAuth endpoint with credentials.
   """
   @spec make_oauth_token_request(map, map, list) ::
-    {:ok, HTTPoison.Response.t} | {:error, HTTPoison.Error.t}
+          {:ok, HTTPoison.Response.t()} | {:error, HTTPoison.Error.t()}
   def make_oauth_token_request(params, cred, options \\ []) do
     endpoint = require_oauth_uri()
     rb = Utils.encode_params(params, cred)
     rh = get_request_headers(nil) |> Map.to_list()
     options = httpoison_request_options() ++ options
+
     case HTTPoison.request(:post, endpoint, rb, rh, options) do
       {:ok, resp} ->
-        {:ok, %{resp | body: Poison.Parser.parse!(resp.body)}}
+        {:ok, %{resp | body: Poison.Parser.parse!(resp.body, %{})}}
+
       resp ->
         resp
     end
@@ -101,30 +103,31 @@ defmodule Dwolla do
   end
 
   def process_response_body(""), do: ""
+
   def process_response_body(body) do
-    case Poison.Parser.parse(body) do
-      {:ok, parsed_body} -> parsed_body
-      {:error, _}        -> {:invalid, body}
-    end
+    Poison.Parser.parse!(body, %{})
   end
 
   defp get_request_headers(nil) do
-    Map.new
+    Map.new()
     |> Map.put("Content-Type", "application/x-www-form-urlencoded")
   end
+
   defp get_request_headers(access_token) do
-    Map.new
+    Map.new()
     |> Map.put("Authorization", "Bearer #{access_token}")
     |> Map.put("Accept", "application/vnd.dwolla.v1.hal+json")
     |> Map.put("Content-Type", "application/vnd.dwolla.v1.hal+json")
   end
 
   defp require_dwolla_credentials do
-    case{get_client_id(), get_client_secret()} do
+    case {get_client_id(), get_client_secret()} do
       {:not_found, _} ->
         raise MissingClientIdError
+
       {_, :not_found} ->
         raise MissingClientSecretError
+
       {client_id, client_secret} ->
         %{client_id: client_id, client_secret: client_secret}
     end
@@ -155,5 +158,4 @@ defmodule Dwolla do
   defp get_client_secret do
     Application.get_env(:dwolla, :client_secret) || :not_found
   end
-
 end
--- lib/dwolla/utils.ex
+++ lib/dwolla/utils.ex
@@ -8,7 +8,7 @@ defmodule Dwolla.Utils do
   @doc """
   Encodes HTTP request.
   """
-  @spec encode_params(map, map) :: String.t
+  @spec encode_params(map, map) :: String.t()
   def encode_params(params, cred \\ %{}) do
     params
     |> Map.merge(cred)
@@ -18,15 +18,19 @@ defmodule Dwolla.Utils do
 
   defp pair({key, value}) do
     param_name = to_string(key) |> URI.encode_www_form()
+
     param_value =
       cond do
         is_map(value) ->
           Poison.encode!(value)
+
         is_list(value) ->
           Enum.map_join(value, "|", fn x -> x end) |> URI.encode_www_form()
+
         true ->
           to_string(value) |> URI.encode_www_form()
       end
+
     "#{param_name}=#{param_value}"
   end
 
@@ -41,10 +45,12 @@ defmodule Dwolla.Utils do
   end
 
   defp do_validate_params(_param_fields, []), do: :ok
-  defp do_validate_params(param_fields, [field|t]) do
+
+  defp do_validate_params(param_fields, [field | t]) do
     case field in param_fields do
       true ->
         do_validate_params(param_fields, t)
+
       _ ->
         :error
     end
@@ -54,19 +60,22 @@ defmodule Dwolla.Utils do
   Converts keys in Dwolla response to Elixir-friendly snake case.
   """
   def to_snake_case(response) when is_binary(response), do: response
+
   def to_snake_case(response) when is_map(response) do
     response
     |> Map.to_list()
     |> Stream.map(fn {k, v} ->
-        cond do
+      cond do
         is_map(v) ->
           {Recase.to_snake(k), to_snake_case(v)}
+
         is_list(v) ->
           {Recase.to_snake(k), Enum.map(v, &to_snake_case/1)}
+
         true ->
           {Recase.to_snake(k), v}
-        end
-      end)
+      end
+    end)
     |> Enum.into(%{})
   end
 
@@ -78,14 +87,15 @@ defmodule Dwolla.Utils do
     |> Map.to_list()
     |> Stream.map(fn {k, v} -> {to_string(k), v} end)
     |> Stream.map(fn {k, v} ->
-        if is_map(v) do
-          {key_to_camel_case(k), to_camel_case(v)}
-        else
-          {key_to_camel_case(k), v}
-        end
-      end)
+      if is_map(v) do
+        {key_to_camel_case(k), to_camel_case(v)}
+      else
+        {key_to_camel_case(k), v}
+      end
+    end)
     |> Enum.into(%{})
   end
+
   def to_camel_case(payload) do
     payload
   end
@@ -101,23 +111,28 @@ defmodule Dwolla.Utils do
   @doc """
   Handles HTTP response from Dwolla.
   """
-  @spec handle_resp({:ok, HTTPoison.Response.t} | {:error, HTTPoison.Error.t}, atom) ::
-    {:ok, any} | {:error, HTTPoison.Error.t | Dwolla.Errors.t | any}
+  @spec handle_resp({:ok, HTTPoison.Response.t()} | {:error, HTTPoison.Error.t()}, atom) ::
+          {:ok, any} | {:error, HTTPoison.Error.t() | Dwolla.Errors.t() | any}
   def handle_resp({:ok, %{body: {:invalid, body}}}, _schema) do
     {:error, body}
   end
+
   def handle_resp({:ok, %{status_code: 200, body: %{"error" => _} = body}}, _schema) do
     {:error, %Dwolla.Errors{code: body["error"], message: body["error_description"]}}
   end
+
   def handle_resp({:ok, %{status_code: code, body: ""} = resp}, _schema) when code in 200..201 do
     {:ok, get_resource_id_from_headers(resp.headers)}
   end
+
   def handle_resp({:ok, %{status_code: code} = resp}, schema) when code in 200..201 do
     {:ok, map_body(resp.body, schema)}
   end
+
   def handle_resp({:ok, resp}, _schema) do
     {:error, format_error(resp.body)}
   end
+
   def handle_resp({:error, error}, _schema) do
     {:error, error}
   end
@@ -125,45 +140,58 @@ defmodule Dwolla.Utils do
   defp map_body(%{"_embedded" => %{"customers" => customers}}, schema) do
     Enum.map(customers, &map_body(&1, schema))
   end
+
   defp map_body(%{"_embedded" => %{"funding-sources" => funding_sources}}, schema) do
     Enum.map(funding_sources, &map_body(&1, schema))
   end
+
   defp map_body(%{"_embedded" => %{"transfers" => transfers}}, schema) do
     Enum.map(transfers, &map_body(&1, schema))
   end
+
   defp map_body(%{"_embedded" => %{"webhook-subscriptions" => webhook_subs}}, schema) do
     Enum.map(webhook_subs, &map_body(&1, schema))
   end
+
   defp map_body(%{"_embedded" => %{"webhooks" => webhooks}}, schema) do
     Enum.map(webhooks, &map_body(&1, schema))
   end
+
   defp map_body(%{"_embedded" => %{"retries" => retries}}, schema) do
     Enum.map(retries, &map_body(&1, schema))
   end
+
   defp map_body(%{"_embedded" => %{"events" => events}}, schema) do
     Enum.map(events, &map_body(&1, schema))
   end
+
   defp map_body(%{"_embedded" => %{"documents" => documents}}, schema) do
     Enum.map(documents, &map_body(&1, schema))
   end
+
   defp map_body(body, :customer) do
     body
     |> to_snake_case()
-    |> Poison.Decode.decode(as: %Dwolla.Customer{})
+    |> Poison.Decode.transform(%{as: %Dwolla.Customer{}})
   end
+
   defp map_body(body, :funding_source) do
     body
     |> to_snake_case()
-    |> Poison.Decode.decode(as: %Dwolla.FundingSource{})
+    |> Poison.Decode.transform(%{as: %Dwolla.FundingSource{}})
   end
+
   defp map_body(%{"_links" => links} = body, :transfer) do
     transfer =
       body
       |> to_snake_case()
-      |> Poison.Decode.decode(as: %Dwolla.Transfer{
+      |> Poison.Decode.transform(%{
+        as: %Dwolla.Transfer{
           amount: %Dwolla.Transfer.Amount{},
-          metadata: %Dwolla.Transfer.Metadata{}}
-        )
+          metadata: %Dwolla.Transfer.Metadata{}
+        }
+      })
+
     can_cancel = Map.has_key?(links, "cancel")
     [source_resource, source_resource_id] = get_transfer_source_from_body(body)
     [dest_resource, dest_resource_id] = get_transfer_destination_from_body(body)
@@ -177,54 +205,67 @@ defmodule Dwolla.Utils do
     |> Map.put(:source_funding_source_id, source_funding_source_id)
     |> Map.put(:can_cancel, can_cancel)
   end
+
   defp map_body(body, :event) do
     event =
       body
       |> to_snake_case()
-      |> Poison.Decode.decode(as: %Dwolla.Event{})
+      |> Poison.Decode.transform(%{as: %Dwolla.Event{}})
+
     resource = get_resource_from_body(body)
     %{event | resource: resource}
   end
+
   defp map_body(body, :webhook_subscription) do
     body
     |> to_snake_case()
-    |> Poison.Decode.decode(as: %Dwolla.WebhookSubscription{})
+    |> Poison.Decode.transform(%{as: %Dwolla.WebhookSubscription{}})
   end
+
   defp map_body(body, :webhook) do
     body
     |> to_snake_case()
-    |> Poison.Decode.decode(as: %Dwolla.Webhook{
-        attempts: [%Dwolla.Webhook.Attempt{
-          request: %Dwolla.Webhook.Attempt.Request{},
-          response: %Dwolla.Webhook.Attempt.Response{}
-        }]
-      })
+    |> Poison.Decode.transform(%{
+      as: %Dwolla.Webhook{
+        attempts: [
+          %Dwolla.Webhook.Attempt{
+            request: %Dwolla.Webhook.Attempt.Request{},
+            response: %Dwolla.Webhook.Attempt.Response{}
+          }
+        ]
+      }
+    })
   end
+
   defp map_body(body, :retry) do
     body
     |> to_snake_case()
-    |> Poison.Decode.decode(as: %Dwolla.Webhook.Retry{})
+    |> Poison.Decode.transform(%{as: %Dwolla.Webhook.Retry{}})
   end
+
   defp map_body(body, :failure) do
     body
     |> to_snake_case()
-    |> Poison.Decode.decode(as: %Dwolla.Transfer.Failure{})
+    |> Poison.Decode.transform(%{as: %Dwolla.Transfer.Failure{}})
   end
+
   defp map_body(%{"balance" => balance} = body, :balance) do
     body
     |> to_snake_case()
     |> Map.merge(to_snake_case(balance))
-    |> Poison.Decode.decode(as: %Dwolla.FundingSource.Balance{})
+    |> Poison.Decode.transform(%{as: %Dwolla.FundingSource.Balance{}})
   end
+
   defp map_body(body, :token) do
     body
     |> to_snake_case()
-    |> Poison.Decode.decode(as: %Dwolla.Token{})
+    |> Poison.Decode.transform(%{as: %Dwolla.Token{}})
   end
+
   defp map_body(body, :document) do
     body
     |> to_snake_case()
-    |> Poison.Decode.decode(as: %Dwolla.Document{})
+    |> Poison.Decode.transform(%{as: %Dwolla.Document{}})
   end
 
   defp get_transfer_source_from_body(%{"_links" => %{"source" => %{"href" => url}}} = _body) do
@@ -233,17 +274,22 @@ defmodule Dwolla.Utils do
     |> Enum.take(-2)
   end
 
-  defp get_transfer_destination_from_body(%{"_links" => %{"destination" => %{"href" => url}}} = _body) do
+  defp get_transfer_destination_from_body(
+         %{"_links" => %{"destination" => %{"href" => url}}} = _body
+       ) do
     url
     |> String.split("/")
     |> Enum.take(-2)
   end
 
-  defp get_transfer_source_funding_source_from_body(%{"_links" => %{"source-funding-source" => %{"href" => url}}} = _body) do
+  defp get_transfer_source_funding_source_from_body(
+         %{"_links" => %{"source-funding-source" => %{"href" => url}}} = _body
+       ) do
     url
     |> String.split("/")
     |> List.last()
   end
+
   defp get_transfer_source_funding_source_from_body(_) do
     nil
   end
@@ -272,15 +318,17 @@ defmodule Dwolla.Utils do
 
   defp format_error(%{"_embedded" => %{"errors" => errors}} = body) do
     new_errors = Enum.map(errors, &Map.take(&1, ["code", "message", "path"]))
+
     body
     |> Map.drop(["_embedded"])
     |> Map.put("errors", new_errors)
     |> format_error()
   end
+
   defp format_error(body) do
     body
     |> to_snake_case()
-    |> Poison.Decode.decode(as: %Dwolla.Errors{errors: [%Dwolla.Errors.Error{}]})
+    |> Poison.Decode.transform(%{as: %Dwolla.Errors{errors: [%Dwolla.Errors.Error{}]}})
   end
 
   @doc """
@@ -289,18 +337,18 @@ defmodule Dwolla.Utils do
   """
   @spec idempotency_header(map) :: map
   def idempotency_header(idempotency_key) when is_binary(idempotency_key) do
-    Map.new
+    Map.new()
     |> Map.put("Idempotency-Key", idempotency_key)
   end
+
   def idempotency_header(params) do
-    Map.new
+    Map.new()
     |> Map.put("Idempotency-Key", generate_idempotency_key(params))
   end
 
   defp generate_idempotency_key(params) do
     :crypto.hash(:md5, encode_params(params))
-    |> Base.encode16
-    |> String.downcase
+    |> Base.encode16()
+    |> String.downcase()
   end
-
 end
